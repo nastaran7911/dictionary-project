ab3746407501c639056fb4a47475be94b2f2490c
nastaran7911 <nastaran7911@gmail.com>
2025-04-27 15:07:52 +0330

    Added the form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
-
+import Dictionary from "./Dictionary"
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      hello
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Nastaran</small>
+        </footer>
+      </div>
     </div>
   );
 }
